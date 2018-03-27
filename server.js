2f5c3fb89ff16f6ef2c832f517625ab603b62bf7
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-03-27 11:52:32 +0100

    server change for heroku deploy

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ http.createServer( function ( request, response ) {
     request.addListener( 'end', function () {
         file.serve( request, response );
     } ).resume();
-} ).listen( port ); 
+} ).listen( process.env.PORT || port ); 
 
 console.log("Your server is running on http://localhost:8080/");
