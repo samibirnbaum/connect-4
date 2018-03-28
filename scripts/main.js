f0ce533ca5d7c0027a02f57de262b0368f20bc86
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-03-28 16:33:49 +0100

    whose turn complete

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@
 //         return boardHtml;
 //     });
 // };
+
+var redPiece = '<div class="red-piece"></div>';
+var yellowPiece = '<div class="yellow-piece"></div>';
+
+var playerOnesTurn = '<h2 class="text-muted">Player 1 <br> It is your turn</h2>';
+var playerTwosTurn = '<h2 class="text-muted">Player 2 <br> It is your turn</h2>';
+
+var clickCountTotal = 0;
+
+$("button").click(function () {
+    clickCountTotal = clickCountTotal + 1;
+    changeTurnInfo();
+});
+
+
+
+
+var changeTurnInfo = function() {
+    if (clickCountTotal%2 === 0) {
+        $(".turn").append(playerOnesTurn + redPiece);
+    }else{
+        $(".turn").append(playerTwosTurn + yellowPiece);
+    }
+};
